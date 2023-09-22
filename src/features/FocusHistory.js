44a374a1366e943a7b082a7dc6586d99f8391df5
Dennis Anane Amematekpor <dennisamematekpor@gmail.com>
2023-09-22 08:57:52 +0000

    Initial commit

---
+++ src/features/FocusHistory.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { colors } from '../utils/colors';
+import { fontSizes, spacing } from '../utils/sizes';
+
+export const FocusHistory = ({ history }) => {
+  if (!history || !history.length)
+    return (
+      <View style={styles.noFocus}>
+        <Text style={styles.noFocusText}>You haven't focused on anything yet </Text>
+      </View>
+    );
+
+  const renderItem = ({ item }) => <Text style={styles.item}>-{item}</Text>;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Things we've focused on:</Text>
+      <FlatList data={history} renderItem={renderItem} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: spacing.xxl,
+  },
+  noFocus: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  noFocusText:{
+     color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+  item: {
+    fontSize: fontSizes.md,
+    color: colors.white,
+    paddingTop: spacing.md,
+  },
+  title: {
+    color: colors.white,
+    fontSize: fontSizes.md,
+    fontWeight: 'bold',
+  },
+});
